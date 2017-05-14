93a77ac311b051dc7653d5eea460fe42386f24d2
geneRocket <745636062@qq.com>
2017-05-14 11:02:14 +0800

    interrupt

--- vm.c
+++ vm.c
@@ -141,18 +141,168 @@ void switchuvm(struct proc *p) {
 	cpu->ts.esp0 = (uint) p->kstack + KSTACKSIZE;
 	// setting IOPL=0 in eflags *and* iomb beyond the tss segment limit
 	// forbids I/O instructions (e.g., inb and outb) from user space
-	cpu->ts.iomb=(ushort)0xFFFF;
-	ltr(SEG_TSS<<3);
-	lcr3(V2P(p->pgdir));// switch to process's address space
+	cpu->ts.iomb = (ushort) 0xFFFF;
+	ltr(SEG_TSS << 3);
+	lcr3(V2P(p->pgdir));	// switch to process's address space
 	popcli();
 
 }
 
-
 // Allocate one page table for the machine for the kernel address
 // space for scheduler processes.
-void kvmalloc(void)
-{
-	kpgdir=setupkvm();
+void kvmalloc(void) {
+	kpgdir = setupkvm();
 	switchkvm();
 }
+
+// Switch h/w page table register to the kernel−only page table,
+// for when no process is running.
+void switchkvm() {
+	lcr3(V2P(kpgdir));	// switch to the kernel page table
+}
+
+// Allocate page tables and physical memory to grow process from oldsz to
+// newsz, which need not be page aligned.  Returns new size or 0 on error.
+int allocuvm(pde_t *pgdir, uint oldsz, uint newsz) {
+	char *mem;
+	uint a;
+	if (newsz >= KERNBASE)
+		return 0;
+	if (newsz < oldsz)
+		return oldsz;
+	a = PGROUNDUP(oldsz);
+	for (; a < newsz; a += PGSIZE)
+	{
+		mem = kalloc();
+		if (mem == 0) {
+			cprintf("allocuvm out of memory\n");
+			deallocuvm(pgdir,newsz,oldsz);
+			return 0;
+		}
+		memset(mem,0,PGSIZE);
+		if(mappages(pgdir,(char*)a,PGSIZE,V2P(mem),PTE_W|PTE_U)<0)
+		{
+			cprintf("allocuvm out of memory (2)\n");
+			deallocuvm(pgdir,newsz,oldsz);
+			kfree(mem);
+			return 0;
+		}
+	}
+	return newsz;
+}
+
+// Deallocate user pages to bring the process size from oldsz to
+// newsz.  oldsz and newsz need not be page-aligned, nor does newsz
+// need to be less than oldsz.  oldsz can be larger than the actual
+// process size.  Returns the new process size.
+int deallocuvm(pde_t *pgdir, uint oldsz, uint newsz) {
+	pte_t *pte;
+	uint a, pa;
+	if (newsz >= oldsz)
+		return oldsz;
+	a = PGROUNDUP(newsz);
+	for (; a < oldsz; a += PGSIZE)
+	{
+		pte = walkpgdir(pgdir, (char *) a, 0);
+		if (!pte)
+			a = PGADDR(PDX(a)+1,0,0) - PGSIZE;
+		else if ((*pte & PTE_P) != 0) {
+			pa=PTE_ADDR(*pte);
+			if(pa==0)
+				panic("kfree");
+			char *v=P2V(pa);
+			kfree(v);
+			*pte=0;
+		}
+	}
+}
+
+// Load a program segment into pgdir.  addr must be page-aligned
+// and the pages from addr to addr+sz must already be mapped.
+int loaduvm(pde_t *pgdir,char *addr,struct inode *ip,uint offset,uint sz)
+{
+	uint i,pa,n;
+	pte_t *pte;
+	if((uint)addr%PGSIZE!=0)
+		panic("loaduvm: addr must be page aligned");
+	for(i=0;i<sz;i+=PGSIZE)
+	{
+		if((pte=walkpgdir(pgdir,addr+i,0))==0)
+			panic("loaduvm: address should exist");
+		pa=PTE_ADDR(*pte);
+		if(sz-i<PGSIZE)
+			n=sz-i;
+		else
+			n=PGSIZE;
+		if(readi(ip,P2V(pa),offset+i,n)!=n)
+			return -1;
+	}
+	return 0;
+}
+
+// Free a page table and all the physical memory pages
+// in the user part.
+void freevm(pde_t *pgdir)
+{
+	uint i;
+
+	if(pgdir==0)
+		panic("freevm: no pgdir");
+	deallocuvm(pgdir,KERNBASE,0);
+	for(i=0;i<NPDENTRIES;i++)
+	{
+		if(pgdir[i]&PTE_P)
+		{
+			char *v=P2V(PTE_ADDR(pgdir[i]));
+			kfree(v);
+		}
+	}
+	kfree((char*)pgdir);
+}
+
+// Clear PTE_U on a page. Used to create an inaccessible
+// page beneath the user stack.
+void clearpteu(pde_t *pgdir,char *uva)
+{
+	pte_t *pte;
+	pte=walkpgdir(pgdir,uva,0);
+	if(pte==0)
+		panic("clearpteu");
+	*pte &= ~PTE_U;
+}
+
+// Map user virtual address to kernel address.
+char *uva2ka(pde_t *pgdir, char *uva) {
+	pte_t *pte;
+
+	pte = walkpgdir(pgdir, uva, 0);
+	if ((*pte & PTE_P) == 0)
+		return 0;
+	if ((*pte & PTE_U) == 0)
+		return 0;
+	return (char*)P2V(PTE_ADDR(*pte));
+}
+
+// Copy len bytes from p to user address va in page table pgdir.
+// Most useful when pgdir is not the current page table.
+// uva2ka ensures this only works for PTE_U pages.
+int copyout(pde_t *pgdir, uint va, void *p, uint len) {
+	char *buf, *pa0;
+	uint n, va0;
+	buf = (char*) p;
+	while (len > 0) {
+		va0 = (uint) PGROUNDDOWN(va);
+		pa0 = uva2ka(pgdir, (char*) va0);
+		if (pa0 == 0)
+			return -1;
+		n = PGSIZE - (va - va0);
+		if (n > len)
+			n = len;
+		memmove(pa0 + (va - va0), buf, n);
+		len -= n;
+		buf += n;
+		va = va0 + PGSIZE;
+	}
+	return 0;
+}
+
